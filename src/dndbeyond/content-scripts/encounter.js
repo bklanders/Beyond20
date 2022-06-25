bc2adfb2afdb46b95bc728a8f8552c8789b56ae0
Ben Landers <bklanders@gmail.com>
2022-06-25 19:22:06 -0400

    Added serial port controls

--- src/dndbeyond/content-scripts/encounter.js
+++ src/dndbeyond/content-scripts/encounter.js
@@ -1,11 +1,34 @@
 console.log("Beyond20: D&D Beyond Encounter module loaded.");
 
+var led_port;
+var led_writer;
 var settings = getDefaultSettings();
 var last_monster_name = null;
 var last_combat = null;
 var character = null;
+var led_serial_started = false;
+var highlighted_seat;
+
+async function setupBeyondLED() {
+    console.log("BeyondLED Setup Started.");
+    const device_filters = [
+        { usbVendorId: 0x2341, usbProductId: 0x0042 } //Arduino Mega
+    ];
+    led_port = await navigator.serial.requestPort({ device_filters });
+    var port_response = await led_port.open({ baudRate: 9600 }); //open serial port to the device.
+    const textEncoder = new TextEncoderStream();
+    const writableStreamClosed = textEncoder.readable.pipeTo(led_port.writable);
+    led_writer = textEncoder.writable.getWriter();
+    led_serial_started = true;
+    console.log("BeyondLED Setup Complete.");
+}
+
+async function sendPlayerToBeyondLED(player) {
+    await led_writer.write(player);
+}
 
 function documentModified(mutations, observer) {
+
     if (isExtensionDisconnected()) {
         console.log("This extension is DOWN!");
         observer.disconnect();
@@ -54,27 +77,36 @@ function updateCombatTracker() {
         combat,
     };
     console.log("Sending combat update", combat);
+    chrome.runtime.sendMessage(req, resp => beyond20SendMessageFailure(character, resp));
+    sendRollRequestToDOM(req);
 
     if (settings["use-beyond-led"])
     {
+        if (!led_serial_started) {
+            setupBeyondLED();
+            return;
+        }
         var highlight_seat;
         character_seating = settings['beyond-led-seating'].split(',').map(name => name.trim());
         active_character = combat.filter(combatant => (combatant.turn))[0];
         if (active_character.tags.includes("character")) {
-            highlight_seat = character_seating.indexOf(active_character.name) + 1;
-            //SEND SEAT TO LEDS
+            highlight_seat = character_seating.indexOf(active_character.name) + 2; //DM + 0 index
+            if (highlight_seat !== highlighted_seat) {
+                sendPlayerToBeyondLED(highlight_seat);
+                highlighted_seat = highlight_seat;
+            }
             console.log(active_character.name + "'s Turn");            
         }
         else {
-            highlight_seat = "DM";
-            //SEND SEAT TO LEDS
+            highlight_seat = "1";
+            if (highlight_seat !== highlighted_seat) {
+                sendPlayerToBeyondLED(highlight_seat);
+                highlighted_seat = highlight_seat;
+            }
             console.log(active_character.name + "'s Turn");            
         }
         console.log("Highlighting Player Seat: " + highlight_seat);
-
     }
-    chrome.runtime.sendMessage(req, resp => beyond20SendMessageFailure(character, resp));
-    sendRollRequestToDOM(req);
 }
 
 
@@ -100,6 +132,7 @@ function handleMessage(request, sender, sendResponse) {
         alertFullSettings();
     }
 }
+
 
 updateSettings();
 injectCSS(BUTTON_STYLE_CSS);
